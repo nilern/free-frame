137795455b8d73eb11f8bd9ac57eca0f8e5bf6f8
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2020-08-10 18:00:30 +0300

    Initial commit.

---
+++ example/simple/src/simpleexample/core.cljs
@@ -0,0 +1,63 @@
+(ns simpleexample.core
+  (:require [free-frame.core :as fr]
+            [cats.core :refer [mlet]]
+            [fell.state :as st]
+            [clojure.string :as str]))
+
+;;;; # Model
+
+(defn initial-db []
+  {:time (js/Date.)
+   :time-color "#f34"})
+
+;;;; # Events
+
+(def event-handlers
+  {:timer (fn [[_ new-time]]
+            (mlet [db st/get]
+              (st/set (assoc db :time new-time))))
+   :time-color-change (fn [[_ new-color]]
+                        (mlet [db st/get]
+                          (st/set (assoc db :time-color new-color))))})
+
+;;;; # Effects
+
+(defn handle-effects [app eff]
+  (-> eff
+      (fr/run-app-db app)))
+
+;;;; # Views
+
+(defn clock []
+  (let [app (fr/useApplication)
+        time-string (-> @(.-db app) :time .toTimeString (str/split " ") first)]
+    [:div.example-clock {:style {:color (:time-color @(.-db app))}}
+     time-string]))
+
+(defn color-input []
+  (let [app (fr/useApplication)]
+    [:div.color-input
+     "Time color: "
+     [:input {:type "text"
+              :value (:time-color @(.-db app))
+              :on-change #(fr/dispatch app [:time-color-change (-> % .-target .-value)])}]]))
+
+(defn ui []
+  [:div
+   [:h1 "Hello world, it is now"]
+   [clock]
+   [color-input]])
+
+;;;; # Initialization
+
+(defn dispatch-timer-event [app]
+  (let [now (js/Date.)]
+    (fr/dispatch app [:timer now])))
+
+(defn render [app]
+  (fr/render app [ui] (js/document.getElementById "app")))
+
+(defn ^:export run []
+  (let [app (fr/create-application (initial-db) event-handlers handle-effects)]
+    (js/setInterval (fn [] (dispatch-timer-event app)) 1000)
+    (render app)))
---
+++ src/free_frame/core.cljs
@@ -0,0 +1,56 @@
+(ns free-frame.core
+  (:require [reagent.core :as rg]
+            [reagent.dom :as rdom]
+            react
+            [fell.core :as fell]
+            [fell.eff :refer [Pure Impure]]
+            [fell.queue :as q]
+            [fell.state :as st]
+            [fell.lift :refer [lift]]
+            [cats.core :refer [mlet extract]]
+            [cats.data :refer [Pair pair]]
+            [promesa.core :as p]))
+
+(def compiler (rg/create-compiler {:function-components true}))
+
+(def context
+  (let [context (react/createContext. nil)]
+    (set! (.-displayName context) "FreeFrameContext")
+    context))
+
+(deftype Application [db event-handlers effect-handler])
+
+(defn create-application [db event-handlers effect-handler]
+  (Application. (rg/atom db) event-handlers effect-handler))
+
+(defn useApplication [] (react/useContext context))
+
+(defn dispatch [^Application app, event]
+  (let [handle-event (-> app .-event_handlers (get (first event)))
+        handle-effects (.-effect_handler app)]
+    (->> event handle-event (handle-effects app))))
+
+(declare resume-app-db)
+
+(defn- resume-app-db* [app eff]
+  ;; HACK:
+  (loop [eff eff]
+    (condp instance? eff
+      Pure (lift (p/resolved (extract eff)))
+      Impure (let [^Impure eff eff
+                   request (.-request eff)
+                   k (partial q/apply-queue (.-cont eff))]
+               (condp instance? request
+                 st/Get (recur (k @(.-db app)))
+                 st/Set (mlet [:let [^st/Set request request]
+                               _ (lift (p/create (fn [resolve _]
+                                                   (resolve (reset! (.-db app) (.-new_value request))))))]
+                          (resume-app-db* app (k nil)))
+                 (fell/weave eff (pair app nil) resume-app-db))))))
+
+(defn- resume-app-db [^Pair suspension] (resume-app-db* (.-fst suspension) (.-snd suspension)))
+
+(defn run-app-db [eff app] (resume-app-db* app eff))
+
+(defn render [app component container]
+  (rdom/render [:> (.-Provider context) {:value app} component] container compiler))
