3a9878d67a241a0115a6e842e97399694bd9b43f
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2020-09-03 18:44:35 +0300

    Name `task`.

--- example/todomvc/src/todomvc/effects.cljs
+++ example/todomvc/src/todomvc/effects.cljs
@@ -9,9 +9,10 @@
 (def ^:private check-state (state-checker (::db/db db/registry) {:registry db/registry}))
 
 (defn handle [app eff]
-  (->> (-> eff
-           check-state
-           (fr/run-app-db app))
-       (lift/run k/context)
-       (t/fork (fn [err] (throw (js/Error. err)))
-               (fn [res] (assert (nil? res))))))
+  (let [task (->> (-> eff
+                      check-state
+                      (fr/run-app-db app))
+                  (lift/run k/context))]
+    (t/fork (fn [err] (throw (js/Error. err)))
+            (fn [res] (assert (nil? res)))
+            task)))
