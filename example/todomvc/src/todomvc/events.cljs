05a1990424c86e35d11ef83038221a0478e83660
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2020-09-09 16:31:03 +0300

    Add some event handlers to TodoMVC example.

--- example/todomvc/src/todomvc/events.cljs
+++ example/todomvc/src/todomvc/events.cljs
@@ -13,4 +13,24 @@
   {:add-todo (fn [[_ text]]
                (mlet [db st/get
                       :let [id (allocate-next-id (:todos db))]]
-                 (st/set (assoc-in db [:todos id] {:id id :title text :done false}))))})
+                 (st/set (assoc-in db [:todos id] {:id id :title text :done false}))))
+
+   :toggle-done (fn [[_ id]]
+                  (mlet [db st/get]
+                    (st/set (update-in db [:todos id :done] not))))
+
+   :delete-todo (fn [[_ id]]
+                  (mlet [db st/get]
+                    (st/set (update db :todos dissoc id))))
+
+   :save (fn [[_ id title]]
+           (mlet [db st/get]
+             (st/set (assoc-in db [:todos id :title] title))))
+
+   :complete-all-toggle (fn [_]
+                          (mlet [{:keys [todos] :as db} st/get
+                                 :let [done* (not-every? :done (vals todos))
+                                       todos (reduce (fn [todos id] (assoc-in todos [id :done] done*))
+                                                     todos
+                                                     (keys todos))]]
+                            (st/set (assoc db :todos todos))))})
